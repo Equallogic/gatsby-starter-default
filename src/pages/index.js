551bf3d43511dff9754ebbf935a2082635363fe9
mgreavescsi <mgreaves@csicompanies.com>
2022-04-01 18:49:50 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,11 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/stock-vector-letter-l-eleven-logo-design-logo-for-business-statistics-1664400928.jpg
-      width={auto}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="E113V3N"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Hello Cheryl</h1>
+    <p>Look at my cool Node.js site</p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-typescript/">Go to "TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
